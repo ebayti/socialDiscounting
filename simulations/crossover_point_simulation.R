782c081fd168782d0a6fd1ccb79b48a2221301cf
Emre Baytimur <emre.baytimur@protonmail.com>
2025-02-18 17:45:16 +0100

    softmax is implemented but then deleted

--- simulations/crossover_point_simulation.R
+++ simulations/crossover_point_simulation.R
@@ -9,8 +9,9 @@ social_distances <- c(1, 2, 3, 5, 10, 20, 50, 100)  # Social distances
 selfish_amounts <- seq(155, 75, by = -10)  # Selfish amounts (offer_a)
 n_trials <- length(social_distances) * length(selfish_amounts)  # Total trials
 
+# Have different amount foregone for each social distance
 # Generate a single V value for the subject and repeat it for each social distance
-amount_foregone <- rep(rnorm(n_subject, mean = 83, sd = 5), length(social_distances))
+V <- rep(rnorm(1, mean = 83, sd = 5), each = length(social_distances))
 
 # Initialize a data frame to store simulation results (deterministic version)
 simulation_results <- data.frame(
@@ -18,19 +19,18 @@ simulation_results <- data.frame(
   "distance" = integer(n_trials),
   "selfish_amount" = numeric(n_trials),
   "generous_amount" = numeric(n_trials),
-  "b_selected" = logical(n_trials),
   "stringsAsFactors" = FALSE
 )
 
-# Deterministic simulation using a computed crossover point (CP)
+# Deterministic simulation 
 # The model is: v = V / (1 + s * N)
-# The crossover point (CP) is defined as:
-#    CP = generous_amount + V(amount foregone)
-simulate_choices <- function(N, V, s, offers, generous_amount) {
-  # Compute the crossover point (CP)
-  CP <- generous_amount + amount_foregone / (1 + s * N)
-  # For each offered amount, if offer >= CP choose "A"; if offer < CP choose "B"
-  choices <- ifelse(offers >= CP, "A", "B")
+simulate_choices <- function(N, V, s, offers, generous_amount,b = 0.1) {
+  # Compute discounted value
+  v_sim <- generous_amount + V / (1 + s * N)
+  #v_yes <- 1 / (1 + exp(b * (generous_amount - v_sim)))  # Probability of choosing "A"
+  #choices <- ifelse(rbinom(n = 1, size = 1, prob = v_yes), "A", "B")
+  # For each offered amount, if offer >= v_sim choose "A"; if offer < v_sim choose "B"
+  choices <- ifelse(offers >= v_sim, "A", "B")
   return(choices)
 }
 
@@ -39,7 +39,7 @@ trial_idx <- 1
 for (i in seq_along(social_distances)) {
   d <- social_distances[i]
   # Get the deterministic choices for this social distance (for all selfish_amounts)
-  choices <- simulate_choices(d, V[i], s, selfish_amounts, generous_amount)
+  choices <- simulate_choices(d, V[i], s, selfish_amounts, generous_amount,b = 0.1)
   for (j in seq_along(selfish_amounts)) {
     simulation_results$trial[trial_idx] <- trial_idx
     simulation_results$distance[trial_idx] <- d
